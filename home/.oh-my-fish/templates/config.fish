489a7180d9a98a58d3325776063cad47f522d1b0
Crawford III <icharles@amazon.com>
2014-04-29 23:37:46 +0000

    Migraite to oh my fish

--- home/.oh-my-fish/templates/config.fish
+++ home/.oh-my-fish/templates/config.fish
@@ -1,24 +1,15 @@
-
 # Path to your oh-my-fish.
 set fish_path $HOME/.oh-my-fish
 
 # Theme
-set fish_theme simplevi
+set fish_theme robbyrussell
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins rvm vi-mode
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
-
-# Simple alias' that don't warrent a function
-alias apt-get=apt-get -y
-alias vi=vim
-alias c=clear
-alias l=ls
-alias s=ls
