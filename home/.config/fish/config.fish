7afee85c772152154c630ee09681dd50483d6b69
Crawford III <icharles@amazon.com>
2014-05-09 14:27:47 -0700

    Add in a tmux conf file.

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -8,13 +8,19 @@ set fish_theme simplevi
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins rvm vi-mode
+set fish_plugins rvm vi-mode vundle
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
+
+# link in local tools I want
+set fish_user_paths $fish_user_paths ~/tools/AWS-ElasticBeanstalk-CLI-2.6.1/eb/linux/python2.7
+
+# link local bin
+set fish_user_paths $fish_user_paths ~/bin
 
 # Simple alias' that don't warrent a function
 alias apt-get=apt-get -y
